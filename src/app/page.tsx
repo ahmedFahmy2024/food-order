41b386538d26301adae550ae626d5f4bec99afda
Ahmed Fahmy <ahmedmewans2030@gmail.com>
2025-01-15 23:26:48 +0200

    handle caching and higher up schema one step

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import { db } from "@/lib/prisma";
 import BestSellers from "./_components/BestSellers";
 import Hero from "./_components/Hero";
+import { getBestSellers } from "@/server/db/products";
 
 export default async function Home() {
-  const products = await db.product.findMany({
-    include: {
-      sizes: true,
-      extras: true,
-    },
-  });
+  const products = await getBestSellers();
 
   return (
     <main>
---
+++ src/server/db/products.ts
@@ -0,0 +1,17 @@
+import { cache } from "@/lib/cache";
+import { db } from "@/lib/prisma";
+
+export const getBestSellers = cache(
+  () => {
+    const bestSellers = db.product.findMany({
+      include: {
+        sizes: true,
+        extras: true,
+      },
+    });
+
+    return bestSellers;
+  },
+  ["best-sellers"],
+  { revalidate: 3600 }
+);
